530ea311d8ec5ad83e53d794517ab9d41fe37d89
Anton Shemerey <shemerey@gmail.com>
2016-05-03 12:14:56 +0300

    display basick numbers

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  // output result
+  @IBOutlet var result: UILabel!
+  
+  @IBAction func touchDigit(sender: UIButton) {
+    if let digit = sender.currentTitle {
+      result.text = digit
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  }
 }
 
